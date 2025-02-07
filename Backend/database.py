41a968182cf5e561120b407cf1e9d44a99cfc0eb
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-07 10:59:26 +0800

    add database and config

---
+++ Backend/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
+
+DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+
+async_engine = create_async_engine(DATABASE_URL)
+
+AsyncSessionLocal = sessionmaker(
+    bind=async_engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import get_db
 
 app = FastAPI()
 
@@ -6,3 +8,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome to FastAPI!"}
+
+
+@app.get("/check-db")
+def check_db(db: Session = Depends(get_db)):
+    return {"status": "success", "message": "Connected to the database successfully!"}
