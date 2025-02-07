f5040be751042c1e661f2d75682d929851777629
BadmintonAdmin <fufilstudio@gmail.com>
2025-02-07 23:45:40 +0800

    Delete migrations

--- Backend/core/config.py
+++ Backend/core/config.py
@@ -13,6 +13,13 @@ class RunConfig(BaseModel):
 
 class DataBaseConfig(BaseModel):
     url: str = os.getenv("DATABASE_URL")
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Settings(BaseSettings):
--- Backend/core/models/base.py
+++ Backend/core/models/base.py
@@ -1,10 +1,15 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.orm import declared_attr
 from utils import camel_case_to_snake_case
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     _abstract_ = True
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
